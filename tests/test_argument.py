efb60e54596a3eb0d08e455f77a098ef0364493e
lars container <lars@container>
2019-02-06 16:19:13 +0100

    linted the test

--- tests/test_argument.py
+++ tests/test_argument.py
@@ -1,9 +1,8 @@
+"""Argument tests."""
 import os
 import unittest
 
 import sys
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
 
 from applicake.base.apputils import dicts
 from applicake.base.coreutils.arguments import Argument, parse_sysargs
@@ -11,19 +10,26 @@ from applicake.base.coreutils.log import Logger
 from applicake.base.coreutils.keys import Keys, KeyHelp
 from applicake.base.coreutils.info import get_handler
 
+MY_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MY_PATH + '/../')
+
 class Test(unittest.TestCase):
+    """Argument tests."""
     @classmethod
     def setUpClass(cls):
         pass
 
-    def test_parseArgs(self):
-        sys.argv = [ '--INPUT', 'fcuk.ini', '--OUTPUT', 'kufc.ini']
-        defaults, cliargs =  parse_sysargs([])
-        print(cliargs);
+    @classmethod
+    def test_parse_args(cls):
+        """Test to parse arguments."""
+        sys.argv = ['--INPUT', 'fcuk.ini', '--OUTPUT', 'kufc.ini']
+        defaults, cliargs = parse_sysargs([])
+        print(defaults)
+        print(cliargs)
 
     def test1_arg_priority(self):
-        return
-        sys.argv = [ '--INPUT', 'fcuk.ini', '--OUTPUT', 'kufc.ini']
+        """Test the argument priorities. Disabled."""
+        sys.argv = ['--INPUT', 'fcuk.ini', '--OUTPUT', 'kufc.ini']
         # basic arguments for every node
 
         basic_args = [Argument(Keys.INPUT, KeyHelp.INPUT, default=''),
@@ -38,10 +44,14 @@ class Test(unittest.TestCase):
 
         for i, arg in enumerate(app_args):
             if arg.name == Keys.WORKDIR:
-                app_args.insert(i + 1, Argument(Keys.BASEDIR, KeyHelp.BASEDIR, default='.'))
-                app_args.insert(i + 2, Argument(Keys.JOB_ID, KeyHelp.JOB_ID, default=''))
-                app_args.insert(i + 3, Argument(Keys.SUBJOBLIST, KeyHelp.SUBJOBLIST, default=''))
-                app_args.insert(i + 4, Argument(Keys.NAME, KeyHelp.NAME, default=self.__class__.__name__))
+                app_args.insert(i + 1,
+                                Argument(Keys.BASEDIR, KeyHelp.BASEDIR, default='.'))
+                app_args.insert(i + 2,
+                                Argument(Keys.JOB_ID, KeyHelp.JOB_ID, default=''))
+                app_args.insert(i + 3,
+                                Argument(Keys.SUBJOBLIST, KeyHelp.SUBJOBLIST, default=''))
+                app_args.insert(i + 4,
+                                Argument(Keys.NAME, KeyHelp.NAME, default=self.__class__.__name__))
                 break
 
         blub = basic_args + app_args
@@ -49,8 +59,8 @@ class Test(unittest.TestCase):
 
         # construct info from defaults < info < commandlineargs
         inifile = cliargs.get(Keys.INPUT, None)
-        ih = get_handler(inifile)
-        fileinfo = ih.read(inifile)
+        info_fh = get_handler(inifile)
+        fileinfo = info_fh.read(inifile)
         info = dicts.merge(cliargs, dicts.merge(fileinfo, defaults))
 
         # setup logging
@@ -60,14 +70,14 @@ class Test(unittest.TestCase):
         if Keys.DATASET_CODE in info:
             if not isinstance(info[Keys.DATASET_CODE], list):
                 if Keys.MZXML in info and not isinstance(info[Keys.MZXML], list):
-                    log.info("Dataset is %s (%s)" % (info[Keys.DATASET_CODE], os.path.basename(info[Keys.MZXML])))
+                    log.info("Dataset is %s (%s)",
+                             info[Keys.DATASET_CODE], os.path.basename(info[Keys.MZXML]))
                 else:
-                    log.info("Dataset is %s" % info[Keys.DATASET_CODE])
+                    log.info("Dataset is %s", info[Keys.DATASET_CODE])
             else:
-                log.debug("Datasets are %s" % info[Keys.DATASET_CODE])
+                log.debug("Datasets are %s", info[Keys.DATASET_CODE])
         #argObj = Argument( "BLUMMER", "DUMMER BLUMMER HELP", default=1)
         #parse_sysargs(argObj)
-
 
     @classmethod
     def tearDownClass(cls):
