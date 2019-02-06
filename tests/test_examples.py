fbc979ef101d3c752b598f1b0788b929e0f2ec50
lars container <lars@container>
2019-02-06 16:19:56 +0100

    linted the test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,11 @@
+"""Test examples."""
 import unittest
 import sys
+from sys import platform as _platform
 import os
 import tempfile
 import shutil
-from sys import platform as _platform
+
 
 from appliapps.examples.echobasic import EchoBasic
 from appliapps.examples.echowrapped import EchoWrapped
@@ -12,22 +14,29 @@ from appliapps.examples.template import TemplateApp
 
 
 class Test(unittest.TestCase):
+    """Test examples."""
+
     @classmethod
     def setUpClass(cls):
         cls.tdir = tempfile.mkdtemp(dir=".")
         os.chdir(cls.tdir)
-        with open("testfile", "w") as f:
-            f.write("testcontent")
+        with open("testfile", "w") as out_fh:
+            out_fh.write("testcontent")
 
-    def test1_pyecho(self):
+    @classmethod
+    def test1_pyecho(cls):
+        """Test pyecho."""
         sys.argv = ['--COMMENT', 'comment']
         EchoBasic.main()
 
-    def test2_extecho(self):
+    @classmethod
+    def test2_extecho(cls):
+        """Test wrapped app."""
         sys.argv = ['--COMMENT', 'comment']
         EchoWrapped.main()
 
     def test3_cp(self):
+        """Test cp."""
         sys.argv = ["--FILE", "testfile"]
         if _platform == "linux":
             CpApp.main()
@@ -35,7 +44,9 @@ class Test(unittest.TestCase):
             self.assertRaises(SystemExit, CpApp.main)
             os.chmod("testfile", 644)
 
-    def test4_tpl(self):
+    @classmethod
+    def test4_tpl(cls):
+        """Test templates."""
         sys.argv = ['--COMMENT', 'comment', '--WORKDIR', '.']
         TemplateApp.main()
         assert os.path.exists("template_out.tpl")
