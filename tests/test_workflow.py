d034df389398cbf133af037d8e3d534f2e27fbd6
lars container <lars@container>
2019-02-06 16:20:31 +0100

    linted the test

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,10 +1,9 @@
+"""Test workflows."""
 import unittest
 import sys
 import os
 import tempfile
 import shutil
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
 
 from appliapps.examples.cp import CpApp
 from appliapps.examples.echowrapped import EchoWrapped
@@ -14,53 +13,66 @@ from appliapps.flow.merge import Merge
 from appliapps.flow.split import Split
 from appliapps.flow.jobid import Jobid
 
+MY_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, MY_PATH + '/../')
+
 
 class Test(unittest.TestCase):
+    """Test workflows."""
     @classmethod
     def setUpClass(cls):
         cls.tdir = tempfile.mkdtemp(dir=".")
         os.chdir(cls.tdir)
-        with open("input.ini", "w") as f:
-            f.write("""
+        with open("input.ini", "w") as out_fh:
+            out_fh.write("""
 COMMENT = comm,ent
 LOG_STORAGE = memory
 LOG_LEVEL = DEBUG
 FILE = test.txt""")
         open("test.txt", "w").write("""TESTTEXT""")
 
-    def test1_jobid(self):
+    @classmethod
+    def test1_jobid(cls):
+        """Test jobid."""
         sys.argv = ['--INPUT', 'input.ini', '--OUTPUT', 'jobid.ini']
         Jobid.main()
         assert os.path.exists('jobid.ini')
-        assert 'JOB_ID' in open('jobid.ini','r').read()
+        assert 'JOB_ID' in open('jobid.ini', 'r').read()
 
-    def test2_branch(self):
-        sys.argv = ['--INPUT', 'jobid.ini', '--BRANCH', 'forcp.ini','--BRANCH', 'forecho.ini',]
+    @classmethod
+    def test2_branch(cls):
+        """Test brancing."""
+        sys.argv = ['--INPUT', 'jobid.ini', '--BRANCH', 'forcp.ini', '--BRANCH', 'forecho.ini',]
         Branch.main()
 
         assert os.path.exists('forcp.ini')
         assert os.path.exists('forecho.ini')
 
-    def test3_cp(self):
+    @classmethod
+    def test3_cp(cls):
+        """Test copy."""
         sys.argv = ['--INPUT', 'forcp.ini', '--OUTPUT', 'cp.ini']
         CpApp.main()
         assert os.path.exists('cp.ini')
-        assert 'COPY' in open('cp.ini','r').read()
+        assert 'COPY' in open('cp.ini', 'r').read()
 
-    def test4_split(self):
+    @classmethod
+    def test4_split(cls):
+        """Test split."""
         sys.argv = ['--INPUT', 'forecho.ini', '--SPLIT', 'split.ini', '--SPLIT_KEY', 'COMMENT']
         Split.main()
 
         assert os.path.exists('split.ini_0')
-        assert 'comm' in open('split.ini_0','r').read()
-        assert not 'ent' in open('split.ini_0','r').read()
+        assert 'comm' in open('split.ini_0', 'r').read()
+        assert not 'ent' in open('split.ini_0', 'r').read()
 
         assert os.path.exists('split.ini_1')
-        assert not 'comm' in open('split.ini_1','r').read()
-        assert 'ent' in open('split.ini_1','r').read()
+        assert not 'comm' in open('split.ini_1', 'r').read()
+        assert 'ent' in open('split.ini_1', 'r').read()
 
-
-    def test5_echo(self):
+    @classmethod
+    def test5_echo(cls):
+        """Test echoing."""
         sys.argv = ['--INPUT', 'split.ini_0', '--OUTPUT', 'echo.ini_0']
         EchoWrapped.main()
 
@@ -71,19 +83,23 @@ FILE = test.txt""")
 
         os.path.exists('echo.ini_1')
 
-    def test6_merge(self):
+    @classmethod
+    def test6_merge(cls):
+        """Test merging."""
         sys.argv = ['--MERGE', 'echo.ini', '--MERGED', 'merged.ini']
         Merge.main()
         assert os.path.exists('merged.ini_0')
 
-    def test7_collate(self):
-        sys.argv = ['--COLLATE', 'cp.ini', '--COLLATE', 'merged.ini_0','--OUTPUT','collate.ini']
+    @classmethod
+    def test7_collate(cls):
+        """Test collate."""
+        sys.argv = ['--COLLATE', 'cp.ini', '--COLLATE', 'merged.ini_0', '--OUTPUT', 'collate.ini']
         Collate.main()
         assert os.path.exists('collate.ini')
 
-        print(open('collate.ini','r').read())
-        assert 'comm, ent' in open('collate.ini','r').read()
-        assert 'COPY' in open('collate.ini','r').read()
+        print(open('collate.ini', 'r').read())
+        assert 'comm, ent' in open('collate.ini', 'r').read()
+        assert 'COPY' in open('collate.ini', 'r').read()
 
     @classmethod
     def tearDownClass(cls):
