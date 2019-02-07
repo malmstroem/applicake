b6ce6d3dc94fe45ee53b239f72bc59e79e5d7fdc
lars container <lars@container>
2019-02-07 14:04:17 +0100

    linting

--- applicake/base/apputils/dirs.py
+++ applicake/base/apputils/dirs.py
@@ -1,3 +1,4 @@
+"""utils to support directories."""
 import errno
 import os
 import re
@@ -8,6 +9,7 @@ from applicake.base.coreutils import Keys
 
 
 def create_workdir(log, info):
+    """Create the workdir."""
     if not Keys.WORKDIR in info:
         log.debug("No WORKDIR requested")
         return info
@@ -21,27 +23,28 @@ def create_workdir(log, info):
         info[Keys.JOB_ID] = create_unique_jobdir(info[Keys.BASEDIR])
         log.debug("JOB_ID was not set, generated [%s]" % info[Keys.JOB_ID])
 
-    wd = ''
+    workdir = ''
     for key in [Keys.BASEDIR, Keys.JOB_ID]:
         if key in info:
-            wd += info[key] + os.path.sep
+            workdir += info[key] + os.path.sep
 
     if info.get(Keys.SUBJOBLIST, "") != "":
         if not isinstance(info[Keys.SUBJOBLIST], list):
             info[Keys.SUBJOBLIST] = [info[Keys.SUBJOBLIST]]
         for subjob in info[Keys.SUBJOBLIST]:
-            ds = re.sub("\W", "", subjob.split(Keys.SUBJOBSEP)[0]) + '_' + subjob.split(Keys.SUBJOBSEP)[1]
-            wd += ds + os.path.sep
+            dataset = re.sub(r"\W", "", subjob.split(Keys.SUBJOBSEP)[0]) + '_' + subjob.split(Keys.SUBJOBSEP)[1]
+            workdir += dataset + os.path.sep
 
-    wd += info[Keys.NAME] + os.path.sep
-    makedirs_clean(wd)
-    log.debug("Created WORKDIR [%s]" % wd)
-    info[Keys.WORKDIR] = wd
+    workdir += info[Keys.NAME] + os.path.sep
+    makedirs_clean(workdir)
+    log.debug("Created WORKDIR [%s]" % workdir)
+    info[Keys.WORKDIR] = workdir
 
     return info
 
 
 def create_unique_jobdir(basedir):
+    """Creates a unique job directory."""
     # dirname using timestamp, principle from tempfile.mkdtemp()
     #limits: longer foldername if >1 dir/min error on >1000 dir/min
     dirname = time.strftime("%y%m%d%H%M")
@@ -52,19 +55,19 @@ def create_unique_jobdir(basedir):
             os.mkdir(path)
             os.chmod(path, 0o775)
             return dirname + ext
-        except OSError as e:
-            if e.errno == errno.EEXIST:
+        except OSError as error:
+            if error.errno == errno.EEXIST:
                 ext = '-' + str(i)
                 continue  # try again
             raise
     raise Exception("Could not create a unique job directory")
 
 
-def makedirs_clean(wd):
+def makedirs_clean(workdir):
     """like os.makedirs(dir) but cleans lowest folder"""
-    if os.path.exists(wd):
-        shutil.rmtree(wd)
-    makedirs_chmod(wd)
+    if os.path.exists(workdir):
+        shutil.rmtree(workdir)
+    makedirs_chmod(workdir)
 
 
 def makedirs_chmod(newdir, mode=0o775):
