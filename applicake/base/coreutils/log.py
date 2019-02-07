01886613a3d3ba7025d4e536a3264946c55f8e70
lars container <lars@container>
2019-02-07 14:05:46 +0100

    linting

--- applicake/base/coreutils/log.py
+++ applicake/base/coreutils/log.py
@@ -1,8 +1,12 @@
+"""The applicake logger."""
 import logging
 
-class Logger(object):
+class Logger:
+    """The applicake logger class."""
+
     @staticmethod
     def create(level):
+        """Create the logger."""
         logging.basicConfig(format="- %(levelname)s - %(message)s")
         logger = logging.getLogger()
         logger.setLevel(level)
